55a424e1836fc043113c4767a2183bd705b8e9a8
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2023-11-13 10:33:24 +0530

    Create App.js

---
+++ Twitter/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function TweetForm({ addTweet }) {
+  const [tweet, setTweet] = useState('');
+
+  const handleTweetChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (tweet.trim() !== '') {
+      addTweet(tweet);
+      setTweet('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        rows="4"
+        cols="50"
+        placeholder="What's happening?"
+        value={tweet}
+        onChange={handleTweetChange}
+      />
+      <br />
+      <button type="submit">Tweet</button>
+    </form>
+  );
+}
+
+function TweetList({ tweets }) {
+  return (
+    <div>
+      {tweets.map((tweet, index) => (
+        <div key={index}>{tweet}</div>
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const [tweets, setTweets] = useState([]);
+
+  const addTweet = (tweet) => {
+    setTweets([...tweets, tweet]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Twitter Clone</h1>
+      <TweetForm addTweet={addTweet} />
+      <TweetList tweets={tweets} />
+    </div>
+  );
+}
+
+export default App;
