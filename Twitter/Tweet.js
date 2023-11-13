be05dd0337feec50801d0237f465d9f449e936fe
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2023-11-13 18:39:40 +0530

    Update Tweet.js

--- Twitter/Tweet.js
+++ Twitter/Tweet.js
@@ -1,19 +1,50 @@
 import React, { useState } from 'react';
 
-const Tweet = ({ tweet, onLike }) => {
+const Tweet = ({ tweet, onLike, onReply }) => {
   const [liked, setLiked] = useState(false);
+  const [replying, setReplying] = useState(false);
+  const [replyText, setReplyText] = useState('');
 
   const handleLike = () => {
     setLiked(!liked);
-    onLike(tweet.id); // Pass the tweet id to the parent component
+    onLike(tweet.id);
+  };
+
+  const handleReply = () => {
+    setReplying(!replying);
+  };
+
+  const handleReplyTextChange = (e) => {
+    setReplyText(e.target.value);
+  };
+
+  const handleReplySubmit = (e) => {
+    e.preventDefault();
+    onReply(tweet.id, replyText);
+    setReplyText('');
+    setReplying(false);
   };
 
   return (
     <div className="tweet">
       <p>{tweet.text}</p>
-      <button onClick={handleLike}>
-        {liked ? 'Unlike' : 'Like'}
-      </button>
+      <div>
+        <button onClick={handleLike}>{liked ? 'Unlike' : 'Like'}</button>
+        <span>{`Likes: ${tweet.likes}`}</span>
+        <button onClick={handleReply}>Reply</button>
+        <span>{`Replies: ${tweet.replies.length}`}</span>
+      </div>
+      {replying && (
+        <form onSubmit={handleReplySubmit}>
+          <input
+            type="text"
+            value={replyText}
+            onChange={handleReplyTextChange}
+            placeholder="Write your reply"
+          />
+          <button type="submit">Submit Reply</button>
+        </form>
+      )}
     </div>
   );
 };
