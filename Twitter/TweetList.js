9d1c99dec6e6e2cdc972d1d91c772bb51e8b3f77
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2023-11-13 18:43:11 +0530

    Update TweetList.js

--- Twitter/TweetList.js
+++ Twitter/TweetList.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Tweet from './Tweet';
+import PostTweet from './PostTweet';
 
 const TweetList = ({ tweets }) => {
   const [likes, setLikes] = useState({});
   const [replies, setReplies] = useState({});
+  const [newTweets, setNewTweets] = useState([]);
 
   const handleLike = (tweetId) => {
     setLikes((prevLikes) => ({
@@ -12,16 +14,29 @@ const TweetList = ({ tweets }) => {
     }));
   };
 
-  const handleReply = (tweetId, replyText) => {
+  const handleReply = (tweetId, replyText, taggedUsers) => {
     setReplies((prevReplies) => ({
       ...prevReplies,
-      [tweetId]: [...(prevReplies[tweetId] || []), replyText],
+      [tweetId]: [...(prevReplies[tweetId] || []), { text: replyText, taggedUsers }],
     }));
   };
 
+  const handlePostTweet = (tweetText) => {
+    const newTweet = {
+      id: newTweets.length + 1,
+      text: tweetText,
+      likes: 0,
+      replies: [],
+    };
+    setNewTweets((prevTweets) => [newTweet, ...prevTweets]);
+  };
+
+  const allTweets = [...tweets, ...newTweets];
+
   return (
     <div className="tweet-list">
-      {tweets.map((tweet) => (
+      <PostTweet onPost={handlePostTweet} />
+      {allTweets.map((tweet) => (
         <Tweet
           key={tweet.id}
           tweet={tweet}
