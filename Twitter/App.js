624aaefd66b371b6859054074951c08e4129125e
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2023-11-14 11:22:00 +0530

    Update App.js

--- Twitter/App.js
+++ Twitter/App.js
@@ -5,37 +5,39 @@ import PostTweet from './PostTweet';
 import UserProfile from './UserProfile';
 import SearchBar from './SearchBar';
 import Notifications from './Notifications';
+import Messages from './Messages';
 
 function App() {
   const [tweets, setTweets] = useState([]);
   const [userName, setUserName] = useState("YourUsername");
-  const [isVerified, setIsVerified] = useState(true); // Set to true for a verified account
+  const [isVerified, setIsVerified] = useState(true);
   const [notifications, setNotifications] = useState([]);
+  const [messages, setMessages] = useState([]);
 
   const addTweet = (tweet) => {
-    // Update state and check for notifications
     setTweets([...tweets, { id: tweets.length + 1, text: tweet, user: userName, likes: 0, replies: [] }]);
     checkNotifications(tweet);
   };
 
   const checkNotifications = (tweet) => {
-    // Example: Check for notifications based on tweet content
     if (tweet.includes('@YourUsername')) {
       setNotifications([...notifications, `You were mentioned in a tweet: "${tweet}"`]);
     }
-  };
-
-  const addTweet = (tweet) => {
-    setTweets([...tweets, { id: tweets.length + 1, text: tweet, user: userName, likes: 0, replies: [] }]);
   };
 
   const handleEditProfile = (newUserName) => {
     setUserName(newUserName);
   };
 
+  const handleSendMessage = (messageText) => {
+    setMessages([...messages, { sender: userName, text: messageText }]);
+  };
+
   return (
     <div className="app">
       <UserProfile userName={userName} onEditProfile={handleEditProfile} verified={isVerified} />
+      <Notifications notifications={notifications} />
+      <Messages messages={messages} onSendMessage={handleSendMessage} />
       <PostTweet onPost={addTweet} />
       <TweetList tweets={tweets} user={userName} />
     </div>
