f729fb2c7fc89de513c56147919efb9e75c7b12d
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2023-11-14 11:18:20 +0530

    Update App.js

--- Twitter/App.js
+++ Twitter/App.js
@@ -4,11 +4,26 @@ import TweetList from './TweetList';
 import PostTweet from './PostTweet';
 import UserProfile from './UserProfile';
 import SearchBar from './SearchBar';
+import Notifications from './Notifications';
 
 function App() {
   const [tweets, setTweets] = useState([]);
   const [userName, setUserName] = useState("YourUsername");
   const [isVerified, setIsVerified] = useState(true); // Set to true for a verified account
+  const [notifications, setNotifications] = useState([]);
+
+  const addTweet = (tweet) => {
+    // Update state and check for notifications
+    setTweets([...tweets, { id: tweets.length + 1, text: tweet, user: userName, likes: 0, replies: [] }]);
+    checkNotifications(tweet);
+  };
+
+  const checkNotifications = (tweet) => {
+    // Example: Check for notifications based on tweet content
+    if (tweet.includes('@YourUsername')) {
+      setNotifications([...notifications, `You were mentioned in a tweet: "${tweet}"`]);
+    }
+  };
 
   const addTweet = (tweet) => {
     setTweets([...tweets, { id: tweets.length + 1, text: tweet, user: userName, likes: 0, replies: [] }]);
